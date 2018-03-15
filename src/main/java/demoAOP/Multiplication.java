7d018f6d50556f0ce32c619531bfc545be05a81c
Frederic Moal <frederic.moal@univ-orleans.fr>
2018-03-15 15:27:37 +0100

    Ajout classe Multiplication

---
+++ src/main/java/demoAOP/Multiplication.java
@@ -0,0 +1,23 @@
+package demoAOP;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by Fred on 14/03/2017.
+ */
+public class Multiplication extends Expression {
+    private Expression leftExpression;
+    private Expression rightExpression;
+
+    public Multiplication(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public int eval() {
+        System.out.println("Evaluation d'un noeud +");
+        return leftExpression.eval() * rightExpression.eval();
+    }
+}
